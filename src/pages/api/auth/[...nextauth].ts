2d3e1ae8c166893a7b570081375fb8e361f02d10
Lucas Rufo <rifelu25@outlook.com>
2021-12-10 23:33:46 -0300

    Finalizando modulo sobre back-end no front-end

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: {
+                  email: user.email
+                }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      }
+      catch {
+        return false;
+      }
+    },
+  }
+})
